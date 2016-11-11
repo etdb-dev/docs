23ae075991da62be9bd3720c620537dcee16eee6
Raphael Beer <raphael.beer@protonmail.com>
2016-11-11 09:31:59 +0100

    Add: Handle some special cases when adding users

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -6,6 +6,8 @@ const User = require('../db/user.js');
 let authController = {};
 
 authController.getToken = (req, res, next) => {
+  if (!canAccess(res, 'readAPI')) return;
+
   logDebug('Creating token');
   let tokenData = res.locals.tokenPayload;
   let token = jwt.sign({
@@ -22,26 +24,43 @@ authController.getToken = (req, res, next) => {
 };
 
 authController.addUser = (req, res) => {
-  if (!res.locals.tokenPayload.access.manageUsers) {
-    return res.status(401).json({
-      message: 'You don\'t have the permission to add users.'
+  if (!canAccess(res, 'manageUsers')) return;
+
+  let data = req.body;
+  let tokenData = res.locals.tokenPayload;
+  if (tokenData.username === data.username) {
+    logWarn(`${data.username} tried to add himself.`);
+    return res.status(409).json({
+      message: 'Now, why would you want to add yourself again? Try PUT-ing onto this address to update your own data!'
     });
   }
-  let data = req.body;
+
   let user = new User({
     'username': data.username,
-    'password': data.password,
-    'access': {
-      manageUsers: true,
-      readAPI: true,
-      writeAPI: true
-    }
+    'password': data.password
   });
-  user.save().then(() => res.json({
-    message: `${data.username} has been createad`
-  })).catch(() => res.status(409).json({
+
+  user.save().then(() => {
+    let msg = `${data.username} has been createad`;
+    logSuccess(msg + ' by ' + tokenData.username);
+    res.json({
+      message: `${data.username} has been createad`
+    });
+  }).catch(() => res.status(409).json({
     message: `user (${data.username}) already exists`
   }));
+};
+
+let canAccess = (res, accessType) => {
+  let access = res.locals.tokenPayload.access;
+  if (access['isAdmin'] || access[accessType]) {
+    return true;
+  } else {
+    res.status(401).json({
+      message: `You don't have the permission to ${accessType}.`
+    });
+    return false;
+  }
 };
 
 module.exports = authController;
