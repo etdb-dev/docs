6b31d8ecdeb068533fbc7a95b99d0a25784976eb
Raphael Beer <raphael.beer@protonmail.com>
2016-11-16 06:16:40 +0100

    Fix: Missing privilege dropping

--- etdb.js
+++ etdb.js
@@ -5,6 +5,7 @@ const Promise = require('bluebird');
 
 const db = require('./src/db');
 const server = require('./src/server');
+const config = require('./src/config');
 
 require('./src/log')();
 
@@ -13,9 +14,13 @@ Promise.try(() => {
     logInfo('Please run ETdb as root!');
     logInfo('ETdb will drop to the user and group given in config.json.');
     throw new Error('Missing privileges');
+  } else {
+    process.setgid(config.get('gid'));
+    process.setuid(config.get('uid'));
+    logInfo(`Dropped privileges to ${process.getuid()}:${process.getgid()}`);
   }
 })
-.then(db.connect())
+.then(db.connect)
 .then(server.start)
 .catch((err) => {
   logError('Startup sequence was interrupted by an error:');
