a1a4bd6e0a38dfdb000eac55593b3710ecaa642c
Raphael Beer <raphael.beer@protonmail.com>
2016-11-16 05:57:48 +0100

    Change: Rename server.mainApp -> server.apiApp

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const Promise = require('bluebird');
 const bodyParser = require('body-parser');
 
 const config = require('./config');
-const mainApp = require('express')();
+const apiApp = require('express')();
 const routers = require('./routers');
 
 let server = {};
@@ -14,17 +14,17 @@ server.start = () => {
   return new Promise((resolve) => {
     logInfo('Starting API server');
 
-    mainApp.use(bodyParser.json());
+    apiApp.use(bodyParser.json());
     let routerKeys = _.keys(routers);
     _.values(routers).forEach((router, idx) => {
       logVerbose('Registering router: ' + routerKeys[idx]);
-      mainApp.use(router);
+      apiApp.use(router);
     });
 
     let cfg = config.get('server');
     let port = cfg.port || 3000;
     let bind = cfg.bind || '127.0.0.1';
-    mainApp.listen(port, bind, () => {
+    apiApp.listen(port, bind, () => {
       logSuccess(`API server listening on ${bind}:${port}`);
       return resolve();
     });
