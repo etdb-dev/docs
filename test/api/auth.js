3d967b5fded63aea0fc3d8081fb057cb6af6c38f
Raphael Beer <raphael.beer@protonmail.com>
2016-11-19 08:31:33 +0100

    Add: tests for POST /auth

--- test/api/auth.js
+++ test/api/auth.js
@@ -8,25 +8,72 @@ module.exports = () => {
   chai.use(require('chai-http'));
   const expect = chai.expect;
 
+  let testingToken;
+  let testingUser = {
+    username: 'testUser',
+    password: 'testPassword'
+  };
+
+  let testAccessDenial = (done) => {
+    chai.request(baseUrl).get('/auth').end((err) => {
+      expect(err).to.have.status(401);
+      done();
+    });
+  };
+
+  let checkMessage = (expectedMessage, resBody) => {
+    expect(resBody).to.have.property('message');
+    expect(expectedMessage).to.equal(resBody.message);
+  };
+
   describe('/auth', () => {
     describe('GET', () => {
 
-      it('denies access when no creds are given', (done) => {
-        chai.request(baseUrl).get('/auth').end((err) => {
-          expect(err).to.have.status(401);
-          done();
-        });
-      });
+      it('should deny access, when no credentials are given', testAccessDenial);
 
-      it('responds with a JWT if credentials are valid', (done) => {
-        chai.request(baseUrl).get('/auth').auth('bobby', 'bobby').end((err, res) => {
+      it('should respond with a JWT, if credentials are valid', (done) => {
+        chai.request(baseUrl)
+        .get('/auth')
+        .auth('bobby', 'bobby')
+        .end((err, res) => {
           expect(err).to.be.null;
           expect(res).to.have.status(200);
           expect(res.body).to.have.property('token');
+          testingToken = res.body.token;
+          done();
+        });
+      });
+    });
+
+    describe('POST', () => {
+
+      it('should deny access, when no credentials are given', testAccessDenial);
+
+      it('should add a new user', (done) => {
+        chai.request(baseUrl)
+        .post('/auth')
+        .auth('bobby', 'bobby')
+        .send(testingUser)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          checkMessage(`${testingUser.username} has been createad`, res.body);
           done();
         });
       });
 
+      it('should respond with 409 when user already exists', (done) => {
+        chai.request(baseUrl)
+        .post('/auth')
+        .auth('bobby', 'bobby')
+        .send(testingUser)
+        .end((err, res) => {
+          expect(err).to.not.be.null;
+          expect(res).to.have.status(409);
+          checkMessage(`user (${testingUser.username}) already exists`, res.body);
+          done();
+        });
+      });
     });
   });
 };
