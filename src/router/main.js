b831db9869d53e77f1e5fe1860db501e00775eb2
Raphael Beer <raphael.beer@protonmail.com>
2016-12-04 14:00:06 +0100

    Add: static /docs for API docs

--- src/router/main.js
+++ src/router/main.js
@@ -1,9 +1,13 @@
 'use strict';
+
+const express = require('express');
 const mainRouter = require('express-promise-router')();
 
 mainRouter.get('/', (req, res) => {
   res.status(418).end();
   logDebug('Answered / route [c=debug]GET[\\c]');
 });
+
+mainRouter.use('/docs', express.static('docs/api'));
 
 module.exports = mainRouter;
