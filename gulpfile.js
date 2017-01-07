0be202d2ead6afe8b5240eed40a476b674e17f2c
Raphael Beer <raphael.beer@protonmail.com>
2017-01-07 23:55:29 +0100

    Change/Add: behave as git-submodule(1) / apidoc, jsdoc tasks
      To document a project, inherit this repo as submodule
      into a first level directory.
      For example api-server/docs -> docs@SHA1
      Configuration files are loaded from your project's root
      (i.e. ../ relative from docs/)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,52 @@
-#!/usr/bin/env gulp
+'use strict';
 
 const gulp = require('gulp');
+const gutil = require('gulp-util');
+const clean = require('gulp-clean');
 const apidoc = require('gulp-apidoc');
+const exec = require('child_process').exec;
 
-console.log(process.cwd());
-console.log(process.pwd());
+const srcRoot = '../src/';
+
+gulp.task('apidoc', [ 'clean' ], (done) => {
+  apidoc({
+    src: '../src/',
+    dest: './output/api-server/ap',
+    includeFilters: [ '.*\\.js$' ],
+    config: '../',
+    silent: true
+  }, done);
+});
+
+gulp.task('jsdoc', [ 'clean' ], (done) => {
+  var pname = require('../package.json').name;
+
+  let params = {
+    configure: '../.jsdoc',
+    package: '../package.json',
+    readme: '../README.md',
+    destination: './output/', // overwrites setting in .jsdoc
+    recurse: ''
+  };
+
+  let paramString = Object.keys(params).reduce((acc, key) => {
+    acc.push(`--${key} ${params[key]} `);
+    return acc;
+  }, []).join(' ');
+
+  exec(`jsdoc ${paramString}${srcRoot}`, (err, stdOut, stdErr) => {
+    if (err) {
+      gutil.log(pname, gutil.colors.red('JSDoc generation failed...'));
+      gutil.log(pname, gutil.colors.yellow(err));
+      return;
+    }
+    gutil.log(pname, gutil.colors.green('JSDocs created. '));
+    done();
+  });
+});
+
+gulp.task('clean', (done) => {
+  return gulp.src('output/', { read: false }).pipe(clean());
+});
+
+gulp.task('default', [ 'clean', 'jsdoc', 'apidoc' ], (done) => done());
